e9947505df494935d1e019ed56d285766b6a32d7
junha <goodjunha@gmail.com>
2022-12-03 22:12:10 +0900

    ❤️‍🔥:: Project Setting

---
+++ ZAVA/src/main/java/com/zava/zava/ZavaApplication.java
@@ -0,0 +1,13 @@
+package com.zava.zava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZavaApplication.class, args);
+    }
+
+}
---
+++ ZAVA/src/test/java/com/zava/zava/ZavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zava.zava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
